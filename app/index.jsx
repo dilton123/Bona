c1951d3e1a7ece71be3a64383e6a259017516ad5
dilton123 <marcusmakena081@gmail.com>
2024-08-04 02:56:40 +0200

    initial and tab nav changes

---
+++ app/index.jsx
@@ -0,0 +1,16 @@
+import { Text, View } from 'react-native';
+import {Link} from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center
+     bg-white">
+      <Text className="text-3xl font-pblack">Bona!</Text>
+      <StatusBar style='auto' />
+      <Link href="/home" style={{ color: 'blue'}}>
+       Go to Profile</Link>
+    </View>
+  );
+}
+
